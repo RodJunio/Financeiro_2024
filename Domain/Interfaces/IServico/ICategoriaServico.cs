1692b857ba139d69a5d7eacb21ee1098e6ca29dc
Junio Rodrigues <devjuniorodrigues@gmail.com>
2024-06-15 02:33:32 -0300

    feat: foi criado interfaces, repositorios genericos e configuração de dbcontext para acessar banco de dados local

---
+++ Domain/Interfaces/IServico/ICategoriaServico.cs
@@ -0,0 +1,4 @@
+﻿namespace Domain.Interfaces.IServico;
+public interface ICategoriaServico
+{
+}
---
+++ Infra/Configuracao/ContextBase.cs
@@ -0,0 +1,36 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Configuracao;
+public class ContextBase : IdentityDbContext<ApplicationUser>
+{
+    public ContextBase(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    public DbSet<SistemaFinanceiro> SistemaFinanceiro { get; set; }
+    public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiro { get; set; }
+    public DbSet<Categoria> Categoria { get; set; }
+    public DbSet<Despesa> Despesa { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ObterStringConexao());
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder Builder)
+    {
+        Builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+        base.OnModelCreating(Builder);
+    }
+
+    public string ObterStringConexao()
+    {
+        return "Data Source=JUNIO;Initial Catalog=Financeiro_2024;Integrated Security=true";
+    }
+
+}
